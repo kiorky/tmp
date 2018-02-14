90a3954b541bc229a2e6232c1688aa02296f92b4
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-02-14 13:44:37 +0100

    up

--- ciphers.sh
+++ ciphers.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 for cipher in $(ssh -Q cipher) ; do
     for i in 1 2 3 ; do
         echo
@@ -10,3 +11,4 @@ for cipher in $(ssh -Q cipher) ; do
         fi
     done
 done
+# vim:set et sts=4 ts=4 tw=80: 
